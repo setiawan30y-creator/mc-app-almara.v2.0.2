4bdf6f045b202e163b43ff06ca089370917bcd70
setiawan30y-creator <setiawan30y@gmail.com>
2026-09-23 05:51:01 +0700

    feat(erp): add cash closing model

---
+++ app/Models/ErpCashClosing.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ErpCashClosing extends Model
+{
+    protected $table = 'erp_cash_closings';
+
+    protected $fillable = [
+        'closing_no', 'closing_date', 'opening_cash', 'cash_in', 'cash_out', 'expense',
+        'expected_cash', 'physical_cash', 'hanging_amount', 'accounted_cash', 'difference',
+        'status', 'notes', 'closed_by', 'closed_at',
+    ];
+
+    protected $casts = [
+        'closing_date' => 'date',
+        'opening_cash' => 'decimal:2',
+        'cash_in' => 'decimal:2',
+        'cash_out' => 'decimal:2',
+        'expense' => 'decimal:2',
+        'expected_cash' => 'decimal:2',
+        'physical_cash' => 'decimal:2',
+        'hanging_amount' => 'decimal:2',
+        'accounted_cash' => 'decimal:2',
+        'difference' => 'decimal:2',
+        'closed_at' => 'datetime',
+    ];
+}
