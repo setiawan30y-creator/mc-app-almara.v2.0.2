99688e610e855dfee23e7c701194c3590e62626b
setiawan30y-creator <setiawan30y@gmail.com>
2026-09-23 14:50:26 +0700

    Inject redesigned currency master UI

--- app/Http/Middleware/InjectClosingBridge.php
+++ app/Http/Middleware/InjectClosingBridge.php
@@ -19,10 +19,14 @@ class InjectClosingBridge
         $html = $response->getContent();
         $scripts = '<script src="/js/modules/17-erp-closing-bridge.js?v=2cea470"></script>'
             . '<script src="/js/modules/18-closing-harian-status.js?v=9539584"></script>'
-            . '<script src="/js/modules/19-erp-transaction-payment-bridge.js?v=8253ff3"></script>';
+            . '<script src="/js/modules/19-erp-transaction-payment-bridge.js?v=8253ff3"></script>'
+            . '<script src="/js/modules/20-currency-master-ui.js?v=ac0f91bb"></script>';
 
         if (!str_contains($html, '19-erp-transaction-payment-bridge.js')) {
             $html = str_replace('</body>', $scripts . '</body>', $html);
+            $response->setContent($html);
+        } elseif (!str_contains($html, '20-currency-master-ui.js')) {
+            $html = str_replace('</body>', '<script src="/js/modules/20-currency-master-ui.js?v=ac0f91bb"></script></body>', $html);
             $response->setContent($html);
         }
 
