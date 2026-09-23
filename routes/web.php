25522bbe3a72db70db00cfab7cd3e58a490a993c
setiawan30y-creator <setiawan30y@gmail.com>
2026-09-23 15:54:47 +0700

    feat: load denomination transaction bridge

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,15 @@ Route::get('/', function () {
     $html = view('dashboard')->render();
     $currencyMasterCss = '<link rel="stylesheet" href="' . asset('css/currency-master-ui-override.css?v=20260923-2') . '">';
     $iso4217Js = '<script src="' . asset('js/modules/21-iso4217-currency-dropdown.js?v=20260923-1') . '"></script>';
+    $denominationBridgeJs = '<script src="' . asset('js/modules/22-denomination-transaction-bridge.js?v=20260923-1') . '"></script>';
     if (stripos($html, 'currency-master-ui-override.css') === false) {
         $html = str_ireplace('</head>', $currencyMasterCss . "\n</head>", $html);
     }
     if (stripos($html, '21-iso4217-currency-dropdown.js') === false) {
         $html = str_ireplace('</body>', $iso4217Js . "\n</body>", $html);
+    }
+    if (stripos($html, '22-denomination-transaction-bridge.js') === false) {
+        $html = str_ireplace('</body>', $denominationBridgeJs . "\n</body>", $html);
     }
 
     return response($html)->header('Content-Type', 'text/html; charset=UTF-8');
