a6086a2b5414aac52e7ed78810439cf4e51541c5
setiawan30y-creator <setiawan30y@gmail.com>
2026-09-26 14:23:14 +0700

    fix(saas): register tenant middleware aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\EnsureRole;
 use App\Http\Middleware\EnsureSingleSession;
+use App\Http\Middleware\TenantAuthMiddleware;
+use App\Http\Middleware\TenantSubdomainMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => EnsureRole::class,
             'single.session' => EnsureSingleSession::class,
+            'tenant.subdomain' => TenantSubdomainMiddleware::class,
+            'tenant.auth' => TenantAuthMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
